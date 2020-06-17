45ac02de845287446cf5badc6cf8966a27487a70
josh muleshi <jmuleshi2@gmail.com>
2020-06-17 14:29:27 +0100

    i adding the header of my homeActivity and i changed the background of my app

--- app/src/main/java/com/example/toastpage/HomeActivity.kt
+++ app/src/main/java/com/example/toastpage/HomeActivity.kt
@@ -2,11 +2,12 @@ package com.example.toastpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.toastpage.R.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(layout.activity_home)
     }
 }
