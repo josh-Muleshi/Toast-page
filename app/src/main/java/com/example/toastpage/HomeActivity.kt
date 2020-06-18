d20450b2086190c012aada7bca5d5340ab7747db
josh muleshi <jmuleshi2@gmail.com>
2020-06-18 08:49:25 +0100

    Modification de la couleur et des certaines fonctionnalite

--- app/src/main/java/com/example/toastpage/HomeActivity.kt
+++ app/src/main/java/com/example/toastpage/HomeActivity.kt
@@ -2,6 +2,9 @@ package com.example.toastpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.GridView
+import android.widget.Toast
 import com.example.toastpage.R.*
 
 class HomeActivity : AppCompatActivity() {
