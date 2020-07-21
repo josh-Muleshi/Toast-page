9b573937f6c4b9f4422c41b55fc51d48cfd61d21
josh muleshi <jmuleshi2@gmail.com>
2020-07-21 11:04:08 +0100

    Modification de la couleur et des certaines fonctionnalite

--- app/src/main/java/com/example/toastpage/HomeActivity.kt
+++ app/src/main/java/com/example/toastpage/HomeActivity.kt
@@ -1,9 +1,14 @@
 package com.example.toastpage
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.GridView
+import android.widget.ImageView
 import android.widget.Toast
 import com.example.toastpage.R.*
 
@@ -13,4 +18,25 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_home)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.more_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            id.settings -> run {
+                startActivity(Intent(this@HomeActivity, SettingsActivity::class.java))
+                finish()
+                //Toast.makeText(baseContext, "you clicked settings", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return true
+    }
 }
+
